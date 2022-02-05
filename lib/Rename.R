b6afbdff9957e6ca4385e8e606a043229c212211
Spencer.Trinh@kinnate.com <spencer.trinhkinnate.com@SpencerTrinhkinnatecoms-MacBook-Pro.local>
2022-02-04 19:33:56 -0800

    completed gutted clean_kinase_data fx; use of dplyr, tidied/refactored

--- lib/Rename.R
+++ lib/Rename.R
@@ -6,89 +6,181 @@ library(dplyr)
 library(textclean)
 library(tidyr)
 
-clean_kinase_data <- function(HGNC, kinasedata, manual_map) {
-  
-  HGNC[, SYMBOL_UPPER := stringr::str_to_upper(SYMBOL)]
-  
+# regex string generator to find partial matches
+get_match_indices <- function(ls_sp1, ls_sp2, match_values, i) {
+    lower_sp1 <- tolower(substr(ls_sp1[i], 2, nchar(ls_sp1[i])))
+    first_sp1 <- substr(ls_sp1[i], 1, 1)
+    sp2 <- ls_sp2[i]
+    pattern = paste0("(.*[",
+                       toupper(first_sp1), 
+                       "|", 
+                       tolower(first_sp1),
+                       "]", 
+                       lower_sp1,
+                       ".*", sp2, ")"
+                     )
+    match_indices <- grep(pattern = pattern,
+                          ignore.case = F, 
+                          x = match_values 
+                         )
+    # print(paste(i, ":", match_indices))
+    return(match_indices)
+    }
 
-  setkey(kinasedata, "Kinase")
-  
-  # If the Kinase does not have an underscore append an "_Km" to the end.
-  kinasedata[str_count(Kinase, "_")==0, Kinase := paste0(Kinase,"_Km")]
-  
-  # Remove rows with a bracket ( [ ),forward slash ( / ) or hyphen (-).
-  kinasedata <- dplyr::filter(kinasedata, !grepl("\\[", Kinase))
-  kinasedata <- dplyr::filter(kinasedata, !grepl("/", Kinase))
-  kinasedata <- dplyr::filter(kinasedata, !grepl("-", Kinase))
-  
-  # Replace greek symbols with their upper case ASCII equivalent.
-  kinasedata[, No_Greek := str_replace(Kinase, "α", "A")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "β", "B")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "γ", "G")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "δ", "D")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "ε", "E")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "ζ", "Z")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "η", "H")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "θ", "Q")]
-  kinasedata[, No_Greek := str_replace(No_Greek, "ι", "I")]
-  
-  kinasedata[, No_space := str_replace(No_Greek, " ", "_")]
-  
-  ### get the Kinase from the front.
-  kinasedata[, Just_Kinase := sub("_.*", "", No_space)]
-  
-  ### Get ATP value from the end.
-  kinasedata[, ATP := stringi::stri_reverse(sub("_.*", "", stringi::stri_reverse(No_space)))]
-  
-  # First round of trying to match the CRO kinase names to the HGNC name. Match to the HGNC Symbol
-  kinasedata[, UPPER := stringr::str_to_upper(Just_Kinase)]
-  KIN04_symbol_match <- merge(kinasedata, HGNC[,.(SYMBOL_UPPER, HGNC_ID, SYMBOL)], by.x = 'UPPER', by.y = 'SYMBOL_UPPER', all.x = TRUE, all.y = FALSE)
-  KIN04_symbol_nomatch <- KIN04_symbol_match[is.na(HGNC_ID)]
-  KIN04_symbol_match <- na.omit(KIN04_symbol_match, cols = "HGNC_ID")
-  
-  # Second round of trying to match the unmatched CRO kinase names to the HGNC name. Match to any HGNC Alias Symbol.
-  TALL_HGNC <- HGNC %>% separate_rows(ALIAS_SYMBOL, sep = "\\|")
-  TALL_HGNC <- TALL_HGNC %>% separate_rows(PREV_SYMBOL, sep = "\\|")
-  setDT(TALL_HGNC)
-  TALL_HGNC[, ALIAS_UPPER := stringr::str_to_upper(ALIAS_SYMBOL)]
-  TALL_HGNC[, PREV_UPPER := stringr::str_to_upper(PREV_SYMBOL)]
-  KIN04_symbol_nomatch[,HGNC_ID:=NULL]
-  KIN04_symbol_nomatch[,SYMBOL:=NULL]
-  KIN04_alias_match <- merge(KIN04_symbol_nomatch, TALL_HGNC[,.(ALIAS_UPPER, HGNC_ID, SYMBOL)], by.x = 'UPPER', by.y = 'ALIAS_UPPER', all.x = TRUE, all.y = FALSE)
-  KIN04_alias_nomatch <- KIN04_alias_match[is.na(HGNC_ID)]
-  KIN04_alias_match <- na.omit(KIN04_alias_match, cols = "HGNC_ID")
-  
-  # Third round of trying to match the unmatched CRO kinase names to the HGNC name. Match to any HGNC Previous Symbol.
-  KIN04_alias_nomatch[,HGNC_ID:=NULL]
-  KIN04_alias_nomatch[,SYMBOL:=NULL]
-  KIN04_prev_match <- merge(KIN04_alias_nomatch, TALL_HGNC[,.(PREV_UPPER, HGNC_ID, SYMBOL)], by.x = 'UPPER', by.y = 'PREV_UPPER', all.x = TRUE, all.y = FALSE)
-  KIN04_prev_nomatch <- KIN04_prev_match[is.na(HGNC_ID)]
-  KIN04_prev_match <- na.omit(KIN04_prev_match, cols = "HGNC_ID")
-  
-  # Manually mapped the left over unmatched.  TODO:(This needs to be improved upon.)
-  KIN04_prev_nomatch[,HGNC_ID:=NULL]
-  KIN04_prev_nomatch[,SYMBOL:=NULL]
-  KIN04_manual_match <- merge(KIN04_prev_nomatch, manual_map[,.(UPPER_KINASE, HGNC_ID, SYMBOL=GENE_SYMBOL)], by.x = 'UPPER', by.y = 'UPPER_KINASE', all.x = TRUE, all.y = FALSE)
-  
-  # Stack the matched table back into one.
-  l = list(KIN04_symbol_match, KIN04_alias_match, KIN04_prev_match, KIN04_manual_match)
-  matches <- rbindlist(l, use.names=TRUE, fill = TRUE)
-  setkey(matches, "HGNC_ID")
-  renamed <- unique(matches)
-
-  # Keep just a few columns.
-  kinome_data <- renamed[, .(HGNC_Symbol=SYMBOL, CRO_Kinase=Just_Kinase, ATP, Result)]
-  
-  # Order the table by the kinase symbol.
-  setorder(kinome_data, HGNC_Symbol)
-  
-  # Tag each result with a bin value.
-  kinome_data <- kinome_data %>% mutate(bin_25 = case_when(Result <= 25 ~ "25", Result <=  50 ~ "50", Result <= 75 ~ "75", Result <= 90 ~ "90", Result > 90 ~ "100"))
-  kinome_data <- kinome_data %>% mutate(bin_10 = case_when(Result <= 10 ~ "10", Result <=  20 ~ "20", Result <= 30 ~ "30", 
-                                                           Result <= 40 ~ "40", Result <=  50 ~ "50", Result <= 60 ~ "60", 
-                                                           Result <= 70 ~ "70", Result <=  80 ~ "80", Result <= 90 ~ "90", 
-                                                           Result > 90 ~ "100"))
-  return(data.table(kinome_data))
-
+# main call to loop thru indices for a 1 index match
+map_regex_match <- function(df, HGNC, i) {
+    ls_sp1 <- df$sp1
+    ls_sp2 <- df$sp2
+    name_match_indices <- get_match_indices(ls_sp1, ls_sp2, HGNC$NAME, i)
+    if (length(name_match_indices) == 1) {
+        return(name_match_indices)
+    } else {
+        if (length(name_match_indices > 1)) {
+            for (j in name_match_indices) {
+                # print(j)
+                # print(HGNC$NAME[j])
+                alias_match_indices <- get_match_indices(ls_sp1, ls_sp2, HGNC$ALIAS_NAME[j], i)
+                if (length(alias_match_indices) == 1) {
+                    return(j)
+                } else {
+                    prev_match_indices <- get_match_indices(ls_sp1, ls_sp2, HGNC$PREV_NAME[j], i)
+                    if (length(prev_match_indices) == 1) {
+                        return(j)
+                    }
+                }
+            }
+        }
+    }
+    return(NA)
 }
 
+
+clean_kinase_data <- function(HGNC, kinasedata, manual_map) {
+
+				ht <- HGNC %>%
+						select(SYMBOL_UPPER, HGNC_ID, SYMBOL) 
+
+				df <- kinasedata %>% 
+						arrange(Kinase) %>%
+						filter(!grepl(pattern = "\\[", x = Kinase), 
+									 !grepl(pattern = "/", x = Kinase),
+									 !grepl(pattern = "-", x = Kinase)) %>%
+						mutate(No_Greek =  case_when(
+							Kinase == "α" ~ "A",
+							Kinase == "β" ~ "B",
+							Kinase == "γ" ~ "G",
+							Kinase == "δ" ~ "D",
+							Kinase == "ε" ~ "E",
+							Kinase == "ζ" ~ "Z",
+							Kinase == "η" ~ "H", 
+							Kinase == "θ" ~ "Q",
+							Kinase == "ι" ~ "I",
+							TRUE ~ Kinase
+								)              
+						) %>%
+						mutate(No_Space = str_replace(No_Greek, " ", "_"),
+									 Just_Kinase = sub("_.*", "", No_Space),
+									 Just_Kinase = stringr::str_to_upper(Just_Kinase)) %>%
+						separate(No_Space, into = c('sp1', 'sp2', 'sp3'), extra = 'drop', remove = FALSE, fill="right") %>%
+						merge(ht, by.x = 'Just_Kinase', by.y = 'SYMBOL_UPPER', all.x = TRUE, all.y = FALSE)
+
+				# assign Aurora A/B/C given that it has 2 elements after splitting, i.e. Aurora A
+				dt <- df %>% filter(is.na(SYMBOL), !is.na(sp2), nchar(sp2) < 2) #, !grepl("\\(", Kinase)) 
+				loop_num <- nrow(dt)
+				for (i in seq(1, loop_num)) {
+						idx <- map_regex_match(dt, HGNC[, .(NAME, ALIAS_NAME, PREV_NAME)], i)
+						if (!is.na(idx)) {
+								df <- df %>% mutate(HGNC_ID = replace(HGNC_ID, 
+																											Result == dt$Result[i], 
+																											HGNC$HGNC_ID[idx]),
+																		SYMBOL = replace(SYMBOL,
+																										 Result == dt$Result[i],
+																										 HGNC$SYMBOL[idx])
+																	 ) 
+								}
+				}
+
+				df_sym_nomatch <- df %>% 
+						filter(is.na(HGNC_ID))
+
+				df_sym_match <- df %>% 
+						filter(!is.na(HGNC_ID))  
+
+				SEC_HGNC <- HGNC %>% separate_rows(ALIAS_SYMBOL, sep = "\\|") %>% 
+														separate_rows(PREV_SYMBOL, sep = "\\|") %>%
+														mutate(ALIAS_UPPER = stringr::str_to_upper(ALIAS_SYMBOL), 
+																	 PREV_UPPER = stringr::str_to_upper(PREV_SYMBOL))
+
+				df_sym_nomatch <- df_sym_nomatch %>%
+												mutate(HGNC_ID = NULL,
+															 SYMBOL = NULL)
+
+				df_alias_match <- merge(df_sym_nomatch, 
+																SEC_HGNC, 
+																by.x = 'Just_Kinase', 
+																by.y = 'ALIAS_UPPER', 
+																all.x = T, 
+																all.y = F
+																)
+
+				df_alias_nomatch <- df_alias_match %>%
+                    filter(is.na(HGNC_ID)) %>%
+                    mutate(HGNC_ID = NULL, 
+                           SYMBOL = NULL)
+
+				df_alias_match <- df_alias_match %>%
+														filter(!is.na(HGNC_ID))
+
+				df_prev_match <- merge(df_alias_nomatch,
+                    SEC_HGNC, 
+                    by.x = 'Just_Kinase', 
+                    by.y = 'PREV_UPPER', 
+                    all.x = T, 
+                    all.y = F
+                    )
+				
+				df_prev_nomatch <- df_prev_match %>%
+                        filter(is.na(HGNC_ID)) %>%
+                        mutate(HGNC_ID = NULL, 
+                               SYMBOL = NULL)
+                               
+				df_prev_match <- df_prev_match %>%
+																filter(!is.na(HGNC_ID))
+				
+				df_mnl_match <- merge(df_prev_nomatch, 
+                      manual_map[,.(UPPER_KINASE, HGNC_ID, SYMBOL=GENE_SYMBOL)], 
+                      by.x = 'Just_Kinase', 
+                      by.y = 'UPPER_KINASE', 
+                      all.x = TRUE, 
+                      all.y = FALSE
+                     )
+
+				# Stack the matched table back into one.
+				l = list(df_sym_match, df_alias_match, df_prev_match, df_mnl_match)
+				matches <- rbindlist(l, use.names=TRUE, fill = TRUE) %>%
+												arrange(HGNC_ID)
+				renamed <- unique(matches)
+
+				# Keep just a few columns.
+				kinome_data <- renamed %>%
+                    select(SYMBOL, Just_Kinase, Result) %>%
+                    mutate(HGNC_SYMBOL=SYMBOL, CRO_Kinase=Just_Kinase) %>%
+                    arrange(HGNC_SYMBOL)
+
+				kinome_data <- kinome_data %>%
+										arrange(desc(Result))
+
+				# Tag each result with a bin value.
+				kinome_data <- kinome_data %>% mutate(bin_25 = case_when(Result <= 25 ~ "25", Result <=  50 ~ "50", Result <= 75 ~ "75", Result <= 90 ~ "90", Result > 90 ~ "100"))
+				kinome_data <- kinome_data %>% mutate(bin_10 = case_when(Result <= 10 ~ "10", Result <=  20 ~ "20", Result <= 30 ~ "30", 
+																																	 Result <= 40 ~ "40", Result <=  50 ~ "50", Result <= 60 ~ "60", 
+																																	 Result <= 70 ~ "70", Result <=  80 ~ "80", Result <= 90 ~ "90", 
+																																	 Result > 90 ~ "100"))
+				kinome_data <- kinome_data %>% 
+												arrange(desc(Result), SYMBOL) %>%
+												distinct(SYMBOL, .keep_all = T)
+												
+				print(head(kinome_data))
+
+				return(kinome_data)
+}
