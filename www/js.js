54889d793a07fe57e7543423cec1020289232392
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-26 15:31:16 -0800

    comment testing

--- www/js.js
+++ www/js.js
@@ -1,38 +1,43 @@
 function showTooltip(evt, text) {
-  let tooltip = document.getElementById("tooltip");
-  tooltip.innerHTML = text;
-  tooltip.style.display = "block";
-  tooltip.style.left = evt.pageX + 2 + "px";
-  tooltip.style.top = evt.pageY + 2 + "px";
+  let tooltip = document.getElementById('tooltip')
+  tooltip.innerHTML = text
+  tooltip.style.display = 'block'
+  tooltip.style.left = evt.pageX + 2 + 'px'
+  tooltip.style.top = evt.pageY + 2 + 'px'
 }
 
 function hideTooltip() {
-  var tooltip = document.getElementById("tooltip");
-  tooltip.style.display = "none";
+  var tooltip = document.getElementById('tooltip')
+  tooltip.style.display = 'none'
 }
 
-Shiny.addCustomMessageHandler("resetFileInputHandlerHide", function(elementId) {      
-				//alert('testing');
-				var id = elementId + "_progress";      
-				var elem = document.getElementById(id);
-				elem.style.visibility = "hidden";
-				elem.firstChild.nextElementSibling.style.width = "0%";
-});
+Shiny.addCustomMessageHandler(
+  'resetFileInputHandlerHide',
+  function (elementId) {
+    //alert('testing');
+    var id = elementId + '_progress'
+    var elem = document.getElementById(id)
+    elem.style.visibility = 'hidden'
+    elem.firstChild.nextElementSibling.style.width = '0%'
+  }
+)
 
-Shiny.addCustomMessageHandler("resetFileInputHandlerShow", function(elementId) {      
-				alert('testing');
-				var id = elementId + "_progress";      
-				var elem = document.getElementById(id);
-				elem.style.visibility = "visible";
-				elem.firstChild.nextElementSibling.style.width = "100%";
-});
+Shiny.addCustomMessageHandler(
+  'resetFileInputHandlerShow',
+  function (elementId) {
+    //alert('testing');
+    var id = elementId + '_progress'
+    var elem = document.getElementById(id)
+    elem.style.visibility = 'visible'
+    elem.firstChild.nextElementSibling.style.width = '100%'
+  }
+)
 
 function toggle(elementId) {
-    var ele = document.getElementById(elementId);
-    if(ele.style.display == "block") {
-            ele.style.display = "none";
-    }
-    else {
-        ele.style.display = "block";
-    }
-} 
+  var ele = document.getElementById(elementId)
+  if (ele.style.display == 'block') {
+    ele.style.display = 'none'
+  } else {
+    ele.style.display = 'block'
+  }
+}
